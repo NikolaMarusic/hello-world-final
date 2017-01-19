ddc085aa17e5eb95f7bb168dc01b66d59651646a
NikolaMarusic <m.niko@live.se>
2017-01-19 11:19:24 +0100

    Added Code

---
+++ hello-world-final/hello-world-final/HelloClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_final
+{
+    class HelloClass
+    {
+        public void write()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hello-world-final/hello-world-final/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloClass newRun = new HelloClass();
+            newRun.write();
+        }
+    }
+}
